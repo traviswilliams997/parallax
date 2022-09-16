cfca26cb28867cf5b97a7a9861669f4342ae9a64
Travis Williams <traviswilliams997@gmail.com>
2022-09-16 17:17:44 -0500

    Create component

---
+++ parallax-react/src/components/TextBox.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TextBox = () => {
+    return (
+        <div className = 'text-box'>
+            <h3> First Class Ticket</h3>
+            <p>
+
+            </p>
+        </div>
+    )
+}
+
+export default TextBox
