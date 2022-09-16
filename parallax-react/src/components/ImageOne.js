5e30b14b3c34f17ddc987a5f00f062d80b0e2d50
Travis Williams <traviswilliams997@gmail.com>
2022-09-16 16:59:36 -0500

    Create component

---
+++ parallax-react/src/components/ImageOne.js
@@ -0,0 +1,14 @@
+import { Parallax } from 'react-parallax';
+import Nasa from '../img/nasa.jpg'
+
+
+const ImageOne = () => (
+    <Parallax bgImage={Nasa} strength={800}>
+        <div className= 'content'>
+            <span className = 'img-txt'>a trip to space </span>
+        </div>
+
+    </Parallax>
+);
+
+export default ImageOne
